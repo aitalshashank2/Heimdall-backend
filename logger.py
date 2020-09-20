70d29469e1351655b99047d762a69c07a997385c
shashank <aitalshashank2@gmail.com>
2020-09-20 16:51:08 +0530

    Basic Workfloe implemented

--- logger.py
+++ logger.py
@@ -1,4 +1,14 @@
 from flask import Flask, request
+import subprocess
+import yaml
+import io
+
+with io.open('config.yml', 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
+
+repo = data_loaded["repo"]
+repo_file = data_loaded["repo_file"]
+destination_file = data_loaded["destination_file"]
 
 app = Flask(__name__)
 
@@ -7,7 +17,10 @@ app = Flask(__name__)
 def logger():
     headers = request.headers
     if(headers["X-GitHub-Event"]=="push"):
-        print("Push Event Detected.")
+        pull = subprocess.Popen(["git", "pull", "origin", "master"], cwd=repo)
+        output, error = pull.communicate()
+        copy = subprocess.Popen(["cp", repo+repo_file, destination_file])
+        output, error = copy.communicate()
     else:
         print("An error occured.")
-    return "Hello World!"
+    return "OK"
