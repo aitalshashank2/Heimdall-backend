ac985a648b399fa81f25deec12fe61835f34dd1b
shashank <aitalshashank2@gmail.com>
2020-09-20 17:23:56 +0530

    Directory structure implemented

--- logger.py
+++ logger.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 import subprocess
 import yaml
 import io
+import os
 
 with io.open('config.yml', 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
 repo = data_loaded["repo"]
-repo_file = data_loaded["repo_file"]
 destination_file = data_loaded["destination_file"]
 
 app = Flask(__name__)
@@ -16,11 +16,20 @@ app = Flask(__name__)
 
 def logger():
     headers = request.headers
-    if(headers["X-GitHub-Event"]=="push"):
+    if headers["X-GitHub-Event"]=="push":
         pull = subprocess.Popen(["git", "pull", "origin", "master"], cwd=repo)
         output, error = pull.communicate()
-        copy = subprocess.Popen(["cp", repo+repo_file, destination_file])
-        output, error = copy.communicate()
+
+        public_keys = os.listdir(repo+"public-keys/")
+
+        f = open(destination_file, "w")
+        for i in public_keys:
+            if i=="instructions.md":
+                continue
+            with open(os.path.join(repo+"public-keys", i), 'r') as tf:
+                f.write(tf.read())
+
     else:
         print("An error occured.")
+        return "An error occured."
     return "OK"
